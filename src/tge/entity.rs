74d848cd3635607e80d7333614bb2436a06567cc
ysyx_22051213-Zhang Yuchi <inchinaxiaofeng@gmail.com>
2024-10-24 21:07:43 +0800

    Update

--- src/tge/entity.rs
+++ src/tge/entity.rs
@@ -1,20 +1,31 @@
+use std::io::{self, Write};
+
 pub struct Entity {
     pub id: usize,
     pub name: String,
-    pub appearance: String, // 形象可以是字符或帧动画
+    pub position: (u16, u16),
+    traits: Vec<Box<dyn EntityTrait>>,
 }
 
 impl Entity {
-    pub fn new(id: usize, name: &str, appearance: &str) -> Self {
-        Self {
+    pub fn new(id: usize, name: &str, position: (u16, u16)) -> Self {
+        Entity {
             id,
             name: name.to_string(),
-            appearance: appearance.to_string(),
+            position,
+            traits: Vec::new(),
         }
+    }
+
+    pub fn with_trait(mut self, traits: Vec<Box<dyn EntityTrait>>) -> Self {
+        self.traits.extend(traits);
+        self
     }
 
     pub fn render(&self) -> String {
         // 渲染实体的逻辑
-        format!("Entity: {} with appearance: {}", self.name, self.appearance)
+        for theTrait in &self.traits {
+            theTrait.apply();
+        }
     }
 }
